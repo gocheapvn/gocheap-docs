c88a283279fd605641f1175014a1a66f973054f2
Minh Phạm Nhật <gocheap.vn@gmail.com>
2025-08-22 15:19:50 +0700

    cap nhap logo

--- site/js/custom.js
+++ site/js/custom.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const logoLink = document.querySelector("a.md-header__button.md-logo");
   if (logoLink) {
-    logoLink.setAttribute("href", "https://gocheap.vn");
+    logoLink.setAttribute("href", "https://gocheap.vn/driverx");
     logoLink.setAttribute("target", "_blank"); // mở tab mới
   }
 });
